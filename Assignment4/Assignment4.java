123a13f1d72042d4cc4e78d20a638ee90218e87b
Sleepwalker423 <60832934+Sleepwalker423@users.noreply.github.com>
2024-10-24 11:56:52 -0400

    Task one complete

--- Assignment4/Assignment4.java
+++ Assignment4/Assignment4.java
@@ -1,1 +1,27 @@
+/**
+ * Author: Charles Walker
+ * File Name: Assignment4.java
+ * Date: 10/23/2024
+ * Purpose: This is the main class for assignment 4 in SE310. Its purpose is to perform the listed
+ * tasks below using 3 seperate classes. 
+ * 1. Create two linked hash sets {"George", "Jim", "John", "Blake", "Kevin", "Michael"} and 
+ *      {"George", "Katie", "Kevin", "Michelle", "Ryan"} and find their union, difference, and 
+ *      intersection. (You can clone the sets to preserve the original sets from being changed by 
+ *      these set methods.)
+ * 2. Write a program that reads words from a text file and displays all the nonduplicate words in 
+ *      ascending order. (Use a TreeSet to hold the words)
+ * 3. Answer the following questions:
+ *      a. Write the code to format number 12345.678 in the United Kingdom locale. Keep two digits after the decimal point.
+ *      b. Write the code to format number 12345.678 in U.S. currency.
+ *      c. Write the code to parse '12,345.678' into a number.
+ * Notes to self: File location: C:\Users\charl\OneDrive\Documents\GitHub\SE320\Assignment4
+ */
+public class Assignment4 {
 
+    public static void main(String[] args){
+
+        Task1 t1 = new Task1();
+
+        t1.testPrint();
+    }
+}
---
+++ Assignment4/Task1.java
@@ -0,0 +1,107 @@
+/**
+ * Author: Charles Walker
+ * File Name: Task1.java
+ * Date: 10/23/2024
+ * Purpose: This class is used to perform the first task of assignment 4. Its task description is:
+ *      1. Create two linked hash sets {"George", "Jim", "John", "Blake", "Kevin", "Michael"} and 
+ *      {"George", "Katie", "Kevin", "Michelle", "Ryan"} and find their union, difference, and 
+ *      intersection. (You can clone the sets to preserve the original sets from being changed by 
+ *      these set methods.)
+ * Notes to self: 
+ * Use addAll() for union, removeAll() for difference, and retainAll() for intersection.
+ * 
+ * All of these methods are inherited from the Set interface. 
+ * 
+ * LinkedHashSet class documentation: https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/LinkedHashSet.html
+ * 
+ * The LinkedHashSet class has a constructor (LinkedHashSet(Collection<? extends E> c)) that can take any child of the 
+ * collection hierarchy. 
+ * 
+ * A set containing only the elements that are present in A or B but not both is called a symmetric difference.
+ */
+
+import java.util.LinkedHashSet;
+
+public class Task1 {
+
+    //Initialize two LinkedHashSets to hold the names
+    LinkedHashSet<String> names1  = new LinkedHashSet<>();
+    LinkedHashSet<String> names2  = new LinkedHashSet<>();
+
+    //Declares three LinkedHashSets to hold the changed sets
+    LinkedHashSet<String> union;
+    LinkedHashSet<String> difference1;//Will contain the difference from Names1 compared to Names2
+    LinkedHashSet<String> difference2;//Will contain the difference from Names2 compared to Names1
+    LinkedHashSet<String> intersection;
+
+
+
+    //Constructor adds names to the original LinkedHashSets;
+    public Task1(){
+
+        names1.add("George");
+        names1.add("Jim");
+        names1.add("John");
+        names1.add("Blake");
+        names1.add("Kevin");
+        names1.add("Michael");
+
+        names2.add("George");
+        names2.add("Katie");
+        names2.add("Kevin");
+        names2.add("Michelle");
+        names2.add("Ryan");
+
+    }
+
+    public void createUnion(){
+
+        union = new LinkedHashSet<>(names1);
+
+        union.addAll(names2);
+    }
+
+    //Each set is loaded with the opposite set and differenceed with the other to show the difference form both 
+    //perspectives. 
+    public void createDifference(){
+
+        difference1 = new LinkedHashSet<>(names1);
+
+        difference2 = new LinkedHashSet<>(names2);
+
+        difference1.removeAll(names2);
+
+        difference2.removeAll(names1);
+    }
+
+    public void createIntersection(){
+
+        intersection = new LinkedHashSet<>(names1);
+
+        intersection.retainAll(names2);
+    }
+
+    public void testPrint(){
+
+        //This statement is used to ensure the originals remain unchanged in initial testing.
+        System.out.println("Names1: " + names1 + "\nNames2: " + names2 + "\n");
+
+        createUnion();
+
+        System.out.println("Union: " + union + "\n");
+
+        createDifference();
+
+        System.out.println("Difference of Names1 from Names2: " + difference1 + "\n");
+
+        System.out.println("Difference of Names2 from Names1: " + difference2 + "\n");
+
+        createIntersection();
+
+        System.out.println("Intersection: " + intersection + "\n");
+
+        //This statement is used to ensure the originals remain unchanged in initial testing.
+        System.out.println("Names1: " + names1 + "\nNames2: " + names2 + "\n");
+        
+    }
+}
